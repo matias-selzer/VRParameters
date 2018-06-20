5cb2283292ad1579ac437de1dca202c00e78bd29
matiasup <matias.selzer@gmail.com>
2018-06-20 16:42:06 -0300

    Adding Plugin to Pixelate and Spawn Points

---
+++ Assets/Scripts/Others/ScreenResolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(12, 12, false);
+		Resolution[] resolutions = Screen.resolutions;
+
+		Debug.Log(resolutions.Length.ToString());
+		foreach(Resolution res in resolutions)
+		{
+			Debug.Log(res.width.ToString() + " x " + res.height.ToString());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Others/SpawnPlayerPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayerPosition : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = spawnPoints [Random.Range (0, spawnPoints.Length)].position;
+		transform.Rotate (new Vector3 (0, Random.Range (0, 360), 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
