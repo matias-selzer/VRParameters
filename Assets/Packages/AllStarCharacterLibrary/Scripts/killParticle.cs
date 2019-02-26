abe77d7a4a68e15cbac965b5af9ab7966037a417
Matias <matias.selzer@gmail.com>
2019-02-26 15:58:21 -0300

    Updated to Unity 2018.3

--- Assets/Packages/AllStarCharacterLibrary/Scripts/killParticle.cs
+++ Assets/Packages/AllStarCharacterLibrary/Scripts/killParticle.cs
@@ -4,14 +4,14 @@ using System.Collections;
 
 public class killParticle : MonoBehaviour 
 {
-	ParticleEmitter pe;
+	//ParticleEmitter pe;
 	public float lifespan;
 	float startTime;
 
 	// Use this for initialization
 	void Start () 
 	{
-		pe = GetComponent<ParticleEmitter>();
+		//pe = GetComponent<ParticleEmitter>();
 		startTime = Time.fixedTime;
 	}
 	
